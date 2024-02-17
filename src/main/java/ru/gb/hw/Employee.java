c7708eae7ada9b8dc9dc6ea3dc19ecdbf1c1a8ba
caregor <64469336+caregor@users.noreply.github.com>
2024-02-17 16:29:44 +0500

    Created classes;

---
+++ src/main/java/ru/gb/hw/Employee.java
@@ -0,0 +1,48 @@
+package ru.gb.hw;
+
+public class Employee {
+    private int employeeId;
+    private String phoneNumber;
+    private String name;
+    private int experience;
+
+    public Employee(int employeeId, String phoneNumber, String name, int experience) {
+        this.employeeId = employeeId;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.experience = experience;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+}
+
---
+++ src/main/java/ru/gb/hw/EmployeeDirectory.java
@@ -0,0 +1,14 @@
+package ru.gb.hw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDirectory {
+    private List<Employee> employees;
+
+    public EmployeeDirectory() {
+        this.employees = new ArrayList<>();
+    }
+
+}
+
